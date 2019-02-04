738d1e47107f567bb72d4b4fd59918d17b794e9c
islamMaruf <marufislam7424@gmail.com>
2019-02-04 16:48:28 +0600

    some modify occur in dashboard panel

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,13 +12,12 @@ dashboard
                         <div class="card-icon">
                             <i class="material-icons">weekend</i>
                         </div>
-                        <p class="card-category">Bookings</p>
+                        <p class="card-category">Total Post</p>
                         <h3 class="card-title">184</h3>
                     </div>
                     <div class="card-footer">
                         <div class="stats">
-                            <i class="material-icons text-danger">warning</i>
-                            <a href="#pablo">Get More Space...</a>
+
                         </div>
                     </div>
                 </div>
@@ -29,12 +28,12 @@ dashboard
                         <div class="card-icon">
                             <i class="material-icons">equalizer</i>
                         </div>
-                        <p class="card-category">Website Visits</p>
+                        <p class="card-category">Total Tags</p>
                         <h3 class="card-title">75.521</h3>
                     </div>
                     <div class="card-footer">
                         <div class="stats">
-                            <i class="material-icons">local_offer</i> Tracked from Google Analytics
+
                         </div>
                     </div>
                 </div>
@@ -45,12 +44,12 @@ dashboard
                         <div class="card-icon">
                             <i class="material-icons">store</i>
                         </div>
-                        <p class="card-category">Revenue</p>
+                        <p class="card-category">Total Categories</p>
                         <h3 class="card-title">$34,245</h3>
                     </div>
                     <div class="card-footer">
                         <div class="stats">
-                            <i class="material-icons">date_range</i> Last 24 Hours
+
                         </div>
                     </div>
                 </div>
@@ -61,16 +60,34 @@ dashboard
                         <div class="card-icon">
                             <i class="fa fa-twitter"></i>
                         </div>
-                        <p class="card-category">Followers</p>
+                        <p class="card-category">Total visitors</p>
                         <h3 class="card-title">+245</h3>
                     </div>
                     <div class="card-footer">
                         <div class="stats">
-                            <i class="material-icons">update</i> Just Updated
+
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-info card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa fa-twitter"></i>
+                        </div>
+                        <p class="card-category">Total authors</p>
+                        <h3 class="card-title">+245</h3>
+                    </div>
+                    <div class="card-footer">
+                        <div class="stats">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </div>
