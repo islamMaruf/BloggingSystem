d4b332901382ee30f2cd3e20cee284f69c927483
islamMaruf <marufislam7424@gmail.com>
2019-02-14 15:18:12 +0600

    user list view created

--- resources/views/admin/User/user-list.blade.php
+++ resources/views/admin/User/user-list.blade.php
@@ -51,7 +51,7 @@
                                         <tr>
                                             <td>{{$i++}}</td>
                                             <td>{{ $user->info->firstName ? $user->info->firstName.' '.$user->info->lastName : 'N/A'}} </td>
-                                            <td>{{}}</td>
+                                            <td>15</td>
                                             <td>15</td>
                                             <td>25</td>
                                             <td>{{$user->posts->count()}}</td>
@@ -92,36 +92,7 @@
 
         });
 
-        function deleteUser(id) {
-            swal({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                type: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonText: 'No, cancel!',
-                confirmButtonClass: 'btn btn-success',
-                cancelButtonClass: 'btn btn-danger',
-                buttonsStyling: false,
-                reverseButtons: true
-            }).then((result) => {
-                if (result.value) {
-                    event.preventDefault();
-                    document.getElementById('delete-form-' + id).submit();
-                } else if (
-                    // Read more about handling dismissals
-                    result.dismiss === swal.DismissReason.cancel
-                ) {
-                    swal(
-                        'Cancelled',
-                        'Your data is safe :)',
-                        'error'
-                    )
-                }
-            })
-        }
+
 
     </script>
     <script src="https://unpkg.com/sweetalert2@7.19.1/dist/sweetalert2.all.js"></script>
